3d07d6abe0742fec4d3bd2e1679db2f8d48f0f44
Matheus Cunha <matheus.cunha@academico.ifpb.edu.br>
2026-03-20 20:38:57 -0300

    Adiciona script tcl de teste

---
+++ script.tcl
@@ -0,0 +1,7 @@
+puts "Ola, Mundo " ;# Comentario.
+# comentario de inicio de linha
+puts {Ola, Mundo }
+puts {Exemplo invalido} #*Erro* 
+puts "Essa é a linha 1; puts "essa é a linha 2"
+puts "Ola, Mundo;"
+puts OlaMundo
